09a9a294d0cb2aeb877007165a3dc4f921d3ae13
Eric McKinney <mckinney99@gmail.com>
2017-11-14 16:59:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,7 @@ class Triangle
 end
 
   def kind
-    if @length1 <= 0 || @length2 <= 0 || @length3 <= 0 || @length1 + @length2 <= @length3
-    || @length1 + @length3  <= @length2 || @length2 + @length3 <= @length1
+    if @length1 <= 0 || @length2 <= 0 || @length3 <= 0 || @length1 + @length2 <= @length3 || @length1 + @length3  <= @length2 || @length2 + @length3 <= @length1
       raise TriangleError
     elsif @length1 == @length2 and @length1 == @length3
       return :equilateral
