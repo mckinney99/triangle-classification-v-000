b4482137b3d8997ad0d19656f03a8a39bca71177
Eric McKinney <mckinney99@gmail.com>
2017-11-14 16:41:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,14 +9,14 @@ class Triangle
 end
 
   def kind(equilateral:, isosceles:, scalene:)
-    if (@length1 <= 0 || @length2 <= 0 || @length3 <= 0) ||
-      (@length2 + @length1 < @length3 || @length2 + @length3 < @lengnth1 ||
+    if (@length1 <= 0 or @length2 <= 0 or @length3 <= 0) or
+      (@length2 + @length1 < @length3 or @length2 + @length3 < @lengnth1 or
       @length1 + @length3 <= @length2)
       raise TriangleError
     end
-    return :equilateral if @length1 == @length2 && @length1 == @length3
-    return :isosceles if @length1 == @length2 || @length1 == @length3 || @length2 == @length3
-    return :scalene if @length1 != @length2 || @length2 != @length3
+    return :equilateral if @length1 == @length2 and @length1 == @length3
+    return :isosceles if @length1 == @length2 or @length1 == @length3 or @length2 == @length3
+    return :scalene if @length1 != @length2 or @length2 != @length3
     end
   end
 
